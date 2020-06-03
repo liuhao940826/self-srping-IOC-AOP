93578ea4e1a281ed48dcf23ca21dcb9b9d2b1580
alan <liuhao2113@aliyun.com>
2020-06-03 14:29:25 +0800

    add comment

--- spring_@Import/src/main/java/com/alan/beans/MyImportBeanDefinitionRegistrar.java
+++ spring_@Import/src/main/java/com/alan/beans/MyImportBeanDefinitionRegistrar.java
@@ -9,15 +9,19 @@ import org.springframework.core.type.AnnotationMetadata;
 
 /**
  * @Author: liuhao
- * @Description:
+ * @Description:  bean对象注册器
  * @Date: Create in 8:31 PM 2019/5/27
  */
 public class MyImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
 
-
+    /**
+     *
+     * @param annotationMetadata
+     * @param registry  注册器需要去注册需要管理的实例类
+     */
     public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry registry) {
 
-        //1.获得一个对象定义管理器
+        //1.获得一个对象定义管理器 将java类注册到spring中
         BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(Teacher.class);
 
         //2.创建当前java类的实例对象
--- spring_@Import/src/main/java/com/alan/beans/MyImportSelecter.java
+++ spring_@Import/src/main/java/com/alan/beans/MyImportSelecter.java
@@ -5,11 +5,16 @@ import org.springframework.core.type.AnnotationMetadata;
 
 /**
  * @Author: liuhao
- * @Description:
+ * @Description: 导入选择器 返回类的名字
  * @Date: Create in 8:26 PM 2019/5/27
  */
 public class MyImportSelecter implements ImportSelector {
 
+    /**
+     *
+     * @param annotationMetadata 指的是import在使用的时候修饰的类的信息 ApplicationImPortSelectorConfig
+     * @return
+     */
     public String[] selectImports(AnnotationMetadata annotationMetadata) {
 
         String[] classNames ={"com.alan.beans.Student"};
